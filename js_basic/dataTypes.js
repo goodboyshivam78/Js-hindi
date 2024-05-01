890a44681af2f473c605b5f7b8fea2e6891019a0
Shivam <167675700+goodboyshivam78@users.noreply.github.com>
2024-05-01 19:54:10 +0000

    Datatypes of Js

---
+++ js_basic/dataTypes.js
@@ -0,0 +1,39 @@
+//DataTypes
+
+let userName = "Krishna"
+let userAge =  18
+let register = false
+let userMobile = null
+let userEducation = undefined
+// or we can also write : let userEducation;
+
+console.table(typeof userName)
+console.table(typeof userAge)
+console.table(typeof register)
+console.table(typeof userMobile)
+console.table(typeof userEducation)
+// typeof is a keyword used to print the data type of the data or variable.
+
+// JavaScript Datatypes.
+
+//Premitive datatypes
+/*
+number
+BigInt
+string
+boolean
+null
+undefined
+*/
+
+//non-Premitive datatypes
+/*
+Arrays
+objects
+*/
+
+// Question:- Why the dataType of null is object 
+
+/*
+Note:- Always give higher priority to your code readiblity
+*/
